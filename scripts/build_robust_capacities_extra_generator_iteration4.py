225b3395f3f02cd6d2982aac906926feb2abbdad
linyang <yang.lin@outlook.de>
2021-02-10 10:21:10 +0100

    fix error in build_optimized_capacities

--- scripts/build_robust_capacities_extra_generator_iteration4.py
+++ scripts/build_robust_capacities_extra_generator_iteration4.py
@@ -140,7 +140,7 @@ if __name__ == "__main__":
 
     networks_dict = {(capacity_year) :
         os.path.join(network_dir, f'elec_s{simpl}_'
-                                  f'{clusters}_ec_l{l}_{opts}_{capacity_year}.nc')
+                                  f'{clusters}_ec_l{l}_{opts}_{capacity_year}_iteration3.nc')
                      for capacity_year in snakemake.config["scenario"]["capacity_years"]
                      for simpl in expand_from_wildcard("simpl")
                      for clusters in expand_from_wildcard("clusters")
